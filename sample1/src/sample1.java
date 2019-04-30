b43f14c0bdd798bea734101a2fb0bc501752ee6b
jifrivly <jifrivly@gmail.com>
2019-04-30 13:47:43 +0530

    old java samples are moved to new repository without old git

---
+++ sample1/src/sample1.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author JIFRI
+ *
+ */
+public class sample1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("ENTER YOUR NAME : ");
+		String s = System.console().readLine();
+		System.out.println(s);
+
+
+		
+		String text=System.console().readLine();    
+		System.out.println("Text is: "+text);
+
+	}
+
+}
